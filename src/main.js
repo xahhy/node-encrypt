52de668e586e5adea7f5c65f877d215a1f959be6
hoyhe <hoyhe@thoughtworks.com>
2019-06-17 12:59:22 +0800

    Init repo

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {encrypt} = require('./util');
+const app = express();
+
+app.use(bodyParser.text({
+    limit: '100mb'
+}));
+
+app.post('/', async function handleEncrypt(req, res, next) {
+    const data = req.body;
+    if (!data || (data.constructor === Object && Object.keys(data).length === 0)) {
+        res.send(400, 'Empty body');
+        return;
+    }
+    const result = encrypt(data);
+    res.send(result);
+});
+
+app.listen(3000, () => console.log('Example app listening on 3000'));
